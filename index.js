36290defe33a4000ff959021611c60695c151705
wachira <wachiraa954@gmail.com>
2022-06-09 15:50:23 +0300

    commit tree c6b3b10f5a025f7db8d9b5cb3ea25f896f3db88d

--- index.js
+++ index.js
@@ -1,1 +1,48 @@
-// Code your solution in this file!
+function distanceFromHqInBlocks(start){
+    const scuberHq = 42;
+    if (start > scuberHq);{
+       return (start - scuberHq);
+    }{
+        return (scuberHq - start);
+    }
+}
+
+function distanceFromHqInFeet (start){
+    return (parseInt(distanceFromHqInBlocks(start), 10) * 264);
+}
+
+function distanceTravelledInFeet (start, destination){
+    if (start > destination){
+        var distance = start - destination;
+    }else{
+        var distance = destination - start;
+    }
+    return (parseInt(distance, 10) * 264)
+}
+
+
+function distanceTravelledInFeet (start, destination){
+    if (start > destination){
+    var distance = start - destination;
+    }else{
+        var distance = destination - start;
+    }
+    return (parseInt((distance), 10) * 264);
+}
+
+
+function calculatesFarePrice (start, destination)
+{
+    let chargableDistance = distanceTravelledInFeet(start, destination)
+    if (chargableDistance <= 400){
+        return 0;
+    }else if(chargableDistance <= 2000){
+        return ((chargableDistance - 400) * 0.02);
+    }else if (chargableDistance <= 2500){
+        return 25;
+    }else{
+        return "cannot travel that far";
+
+    }
+        
+    }
